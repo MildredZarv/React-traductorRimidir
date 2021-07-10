21f6e06b7a5189ae5fff49bdaef91d42af067b8e
Mildred Zaragoza <mildredzarv1@gmail.com>
2021-07-10 14:21:44 -0500

    index.css updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { Header } from "./components/Header";
 import Traductor from "./components/Traductor";
 
 function App() {
   return (
-    <>
-    <div className = 'container'>
-        <Header />
-        <Traductor />
+    <div className = 'container' >
+      <Traductor />
     </div>
-
-    </>
   );
 }
 
--- src/components/Traductor.js
+++ src/components/Traductor.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import { Header } from './Header';
 
 const Traductor = () => {
     const [ text, setTetx ] = useState();
 
     const handleTraducer = ({target}) => {
-        const newText = target.value.replace(/a/gi,'i').replace(/e/gi,'i').replace(/o/gi,'i').replace(/u/gi,'i'); 
+        let newText = target.value.replace(/a/gi,'i').replace(/e/gi,'i').replace(/o/gi,'i').replace(/u/gi,'i'); 
+        newText = newText.replace(/á/gi,'i').replace(/é/gi,'i').replace(/í/gi,'i').replace(/ó/gi,'i').replace(/ú/gi,'i'); 
+        newText = newText.replace(/ä/gi,'i').replace(/ë/gi,'i').replace(/ï/gi,'i').replace(/ö/gi,'i').replace(/ü/gi,'i'); 
         setTetx( newText );
     }
 
@@ -14,7 +17,8 @@ const Traductor = () => {
     }
 
     return (
-        <div>        
+        <div className = 'cardContent' >
+            <Header />       
             <input className = 'textInput' onChange = { handleTraducer } placeholder = 'type something...' id = 'textInput' />
             <p>{ text }</p>
             <button className = 'controls' onClick = { handleClear }>Clear</button>
